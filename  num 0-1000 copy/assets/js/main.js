fab9f27146f7f9f59771aaf764ca9b59012b0d69
Valerio Corda <corda.valerio05@GMAIL.COM>
2021-10-13 12:13:46 +0200

    Count from 0 to 1000 and the other way around

---
+++  num 0-1000 copy/assets/js/main.js
@@ -0,0 +1,5 @@
+//conto da 1000 a 0
+
+for (let i = 1000; i >= 0; i--) {
+    console.log(i);
+}
---
+++  num 0-1000/assets/js/main.js
@@ -0,0 +1,6 @@
+//Stampo tutti i numeri da 0 a 1000
+const num = document.getElementById("number")
+for (let i = 0; i <= 1000; i++) {
+    console.log(i);
+    num.innerHTML = i;
+}
